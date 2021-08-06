e09c60fc00514da16309592948ada1bda54b7b09
yxiong <yxiong@fnz.com>
2021-08-07 01:47:39 +0800

    view post details and add post

--- redux-toolkit-sample/src/features/posts/PostsList.js
+++ redux-toolkit-sample/src/features/posts/PostsList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const PostsList = () => {
     const posts = useSelector(state => state.posts)
@@ -7,12 +8,15 @@ export const PostsList = () => {
     const renderedPosts = posts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
-            <p className='post-content'>{post.content.substring(0, 10)}</p>
+            <p className="post-content">{post.content.substring(0, 10)}</p>
+            <Link to={`/posts/${post.id}`} className="button muted-button">
+                view Post
+            </Link>
         </article>
     ))
 
     return (
-        <section className='posts-list'>
+        <section className="posts-list">
             <h2>Posts</h2>
             {renderedPosts}
         </section>
